40108f6bb9aacd72a342bf35eb11c2db15477b7e
TrollExeYT <bennacho97@outlook.cl>
2024-10-09 18:16:34 -0300

    v0.1

--- scenes/src/utilities/medic_machine/medic_machine_script.gd
+++ scenes/src/utilities/medic_machine/medic_machine_script.gd
@@ -5,9 +5,21 @@ func _ready() -> void:
 	message = $Message
 	message.rotation = 0
 
-func _heal_player() -> void:
-	INGAME.player.health += get_process_delta_time()
 
-func _physics_process(delta: float) -> void:
+func _heal_player() -> void:
+	INGAME.player.health = move_toward(INGAME.player.health,100, 5 * get_process_delta_time())
+	INGAME.camera._change_health(INGAME.player.health)
+
+
+func _physics_process(_delta: float) -> void:
 	if active:
-		_heal_player()
+		if Input.is_action_pressed("p_interact"):
+			_heal_player()
+
+
+func _on_area_body_entered(_body: Node2D) -> void:
+	_change_active()
+
+
+func _on_area_body_exited(_body: Node2D) -> void:
+	_change_active()
--- scenes/src/utilities/utility_class.gd
+++ scenes/src/utilities/utility_class.gd
@@ -5,7 +5,6 @@ var active : bool = false
 var message : Label
 var cost : int
 
-
 func _change_active() -> void:
 	active = not active
 	message.visible = active
